15b2458906191c5b715c211c8c28a5da8530e620
Akram <akram.boukhers@monamipoto.com>
2024-07-08 03:11:37 +0200

    add bearer token authorization for association

--- back-core/src/associations/associations.controller.ts
+++ back-core/src/associations/associations.controller.ts
@@ -1,34 +1,41 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put, UseGuards } from '@nestjs/common';
 import { AssociationsService } from './associations.service';
 import { CreateAssociationDto } from './dto/create-association.dto';
 import { UpdateAssociationDto } from './dto/update-association.dto';
 import { Association } from './association.entity';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Public } from '../auth/decorators/public.decorator';
 
 @Controller('associations')
 export class AssociationsController {
   constructor(private readonly associationsService: AssociationsService) {}
 
   @Get()
+  @Public()
   findAll(): Promise<Association[]> {
     return this.associationsService.findAll();
   }
 
   @Get(':id')
+  @Public()
   findOne(@Param('id') id: string): Promise<Association> {
     return this.associationsService.findOne(+id);
   }
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Body() createAssociationDto: CreateAssociationDto): Promise<Association> {
     return this.associationsService.create(createAssociationDto);
   }
 
   @Put(':id')
+  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() updateAssociationDto: UpdateAssociationDto) {
     return this.associationsService.update(+id, updateAssociationDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.associationsService.remove(+id);
   }
--- back-core/src/auth/auth.service.ts
+++ back-core/src/auth/auth.service.ts
@@ -1,29 +1,29 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { UsersService } from '../users/users.service';
-import * as bcrypt from 'bcrypt';
+  import { Injectable, UnauthorizedException } from '@nestjs/common';
+  import { JwtService } from '@nestjs/jwt';
+  import { UsersService } from '../users/users.service';
+  import * as bcrypt from 'bcrypt';
 
-@Injectable()
-export class AuthService {
-  constructor(
-    private usersService: UsersService,
-    private jwtService: JwtService,
-  ) {}
+  @Injectable()
+  export class AuthService {
+    constructor(
+      private usersService: UsersService,
+      private jwtService: JwtService,
+    ) {}
 
-  async validateUser(email: string, password: string): Promise<any> {
-    const user = await this.usersService.findByEmail(email);
-    if (user && await bcrypt.compare(password, user.password)) {
-      const { password, ...result } = user;
-      return result;
+    async validateUser(email: string, password: string): Promise<any> {
+      const user = await this.usersService.findByEmail(email);
+      if (user && await bcrypt.compare(password, user.password)) {
+        const { password, ...result } = user;
+        return result;
+      }
+      return null;
     }
-    return null;
-  }
 
-  async login(user: any) {
-    const payload = { email: user.email, sub: user.id, roleId: user.roleId };
-    return {
-      ...user,
-      access_token: this.jwtService.sign(payload),
-    };
+    async login(user: any) {
+      const payload = { email: user.email, sub: user.id, roleId: user.roleId };
+      return {
+        ...user,
+        access_token: this.jwtService.sign(payload),
+      };
+    }
   }
-}
