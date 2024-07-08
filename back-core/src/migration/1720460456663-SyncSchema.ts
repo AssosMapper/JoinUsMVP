db0aa0d81efd27aa98d98e9a9df25fdbff15207c
Akram <akram.boukhers@monamipoto.com>
2024-07-08 20:21:03 +0200

    addAssociationIdToUser

--- back-core/src/migration/1720460456663-SyncSchema.ts
+++ back-core/src/migration/1720460456663-SyncSchema.ts
@@ -4,17 +4,32 @@ export class SyncSchema1720460456663 implements MigrationInterface {
     name = 'SyncSchema1720460456663'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`user\` DROP FOREIGN KEY \`FK_72a26c7d9fb2a68bec390c4b130\``);
-        await queryRunner.query(`DROP INDEX \`REL_72a26c7d9fb2a68bec390c4b13\` ON \`user\``);
-        await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`associationId\``);
+        // Désactiver les vérifications de clé étrangère
+        await queryRunner.query(`SET FOREIGN_KEY_CHECKS=0;`);
+
+        // Supprimer la contrainte de clé étrangère si elle existe
+        try {
+            await queryRunner.query(`ALTER TABLE \`association\` DROP FOREIGN KEY \`FK_6879674c7109f0cd3273e057d09\``);
+        } catch (error) {
+            console.log('Foreign key FK_6879674c7109f0cd3273e057d09 does not exist');
+        }
+
+        // Supprimer l'index s'il existe
+        try {
+            await queryRunner.query(`DROP INDEX \`user_id\` ON \`association\``);
+        } catch (error) {
+            console.log('Index user_id does not exist');
+        }
+
+        // Réactiver les vérifications de clé étrangère
+        await queryRunner.query(`SET FOREIGN_KEY_CHECKS=1;`);
+
+        // Ajouter la nouvelle contrainte de clé étrangère
         await queryRunner.query(`ALTER TABLE \`association\` ADD CONSTRAINT \`FK_6879674c7109f0cd3273e057d09\` FOREIGN KEY (\`user_id\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE \`association\` DROP FOREIGN KEY \`FK_6879674c7109f0cd3273e057d09\``);
-        await queryRunner.query(`ALTER TABLE \`user\` ADD \`associationId\` int NULL`);
-        await queryRunner.query(`CREATE UNIQUE INDEX \`REL_72a26c7d9fb2a68bec390c4b13\` ON \`user\` (\`associationId\`)`);
-        await queryRunner.query(`ALTER TABLE \`user\` ADD CONSTRAINT \`FK_72a26c7d9fb2a68bec390c4b130\` FOREIGN KEY (\`associationId\`) REFERENCES \`association\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`CREATE INDEX \`user_id\` ON \`association\` (\`user_id\`)`);
     }
-
 }
--- interfaces/user.ts
+++ interfaces/user.ts
@@ -7,6 +7,7 @@ export interface User {
     password: string;
     confirmPassword: string;
     roleId: number;
+    associationId: number;
     phone?: string;
     address?: string;
     zip?: string;
