97ff324334f8269334e59412c7512be673a80e82
Akram <akram.boukhers@monamipoto.com>
2024-07-08 17:29:14 +0200

    add guard for events.controller

--- back-core/src/events/events.controller.ts
+++ back-core/src/events/events.controller.ts
@@ -1,34 +1,41 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put, UseGuards } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { Event } from './event.entity';
 import { CreateEventDto } from './dto/create-events.dto';
 import { UpdateEventDto } from './dto/update-events.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Public } from '../auth/decorators/public.decorator';
 
 @Controller('events')
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
   @Get()
+  @Public()
   findAll(): Promise<Event[]> {
     return this.eventsService.findAll();
   }
 
   @Get(':id')
+  @Public()
   findOne(@Param('id') id: string): Promise<Event> {
     return this.eventsService.findOne(+id);
   }
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Body() createEventDto: CreateEventDto): Promise<Event> {
     return this.eventsService.create(createEventDto);
   }
 
-  @Patch(':id')
+  @Put(':id')
+  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
     return this.eventsService.update(+id, updateEventDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.eventsService.remove(+id);
   }
