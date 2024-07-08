f1fc39da75cd7ba1d17ad08f54fb9a718bfd3d38
Akram <akram.boukhers@monamipoto.com>
2024-07-08 23:43:25 +0200

    add guards for all routes

--- back-core/src/users/users.controller.ts
+++ back-core/src/users/users.controller.ts
@@ -1,19 +1,24 @@
-import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './user.entity';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Public } from '../auth/decorators/public.decorator';
 
 @Controller('users')
+@UseGuards(JwtAuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @Public()
   findAll(): Promise<User[]> {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @Public()
   findOne(@Param('id') id: string): Promise<User> {
     return this.usersService.findOne(+id);
   }
