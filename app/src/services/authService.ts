ec36c9521396984b22b455ab59cc1cc4bd32f73e
Akram <akram.boukhers@monamipoto.com>
2024-07-22 22:17:37 +0200

    fix store

--- app/src/services/authService.ts
+++ app/src/services/authService.ts
@@ -5,7 +5,7 @@ import { Login } from '@joinus/interfaces';
 const API_URL = process.env.VUE_APP_BACKEND_URL;
 
 const login = async (credentials: Login) => {
-  const response = await axios.post(`${API_URL}/auth/login`, credentials);
+  const response = await axios.post(`${API_URL}/v1/auth/login`, credentials);
   const userStore = useUserStore();
   userStore.loginUser(response.data);
   return response.data;
