2798ee124bc348cfc09aed0b095bb42f2cac9c40
Ahmindache Jawad <jawad.ahmindache@gmail.com>
2024-09-16 02:33:40 +0200

    [BACK] Join & Cancel association application

--- back-core/src/association-applications/entities/association-application.entity.ts
+++ back-core/src/association-applications/entities/association-application.entity.ts
@@ -1,35 +1,34 @@
-import { Entity, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 import { EntityStructure } from '../../utils/structures/entity.structure';
 import { User } from '../../users/entities/user.entity';
 import { Association } from '../../associations/entities/association.entity';
 
 export enum ApplicationStatus {
-    IN_PROGRESS = 0,
-    APPROVED = 1,
-    DENIED = 2,
+  IN_PROGRESS = '0',
+  APPROVED = '1',
+  DENIED = '2',
 }
 
 @Entity()
 export class AssociationApplication extends EntityStructure {
-    @ManyToOne(() => User)
-    @JoinColumn()
-    user: User;
+  @ManyToOne(() => User)
+  @JoinColumn()
+  user: User;
 
-    @ManyToOne(() => Association)
-    @JoinColumn()
-    association: Association;
+  @ManyToOne(() => Association)
+  @JoinColumn()
+  association: Association;
 
-    @Column({
-        type: 'enum',
-        enum: ApplicationStatus,
-        default: ApplicationStatus.IN_PROGRESS,
-    })
-    status: ApplicationStatus;
+  @Column({
+    type: 'enum',
+    enum: ApplicationStatus,
+    default: ApplicationStatus.IN_PROGRESS,
+  })
+  status: ApplicationStatus;
 
-    @Column({ length: 255, nullable: true })
-    applicationAnswer: string;
+  @Column({ length: 255, nullable: true })
+  applicationAnswer: string;
 
-    @Column({ length: 255, nullable: true })
-    applicationQuestion: string;
+  @Column({ length: 255, nullable: true })
+  applicationQuestion: string;
 }
-
