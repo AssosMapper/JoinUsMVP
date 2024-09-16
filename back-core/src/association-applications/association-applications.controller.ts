69bdedc0593ea441a22d359fe02a5649c9ea5dc0
Ahmindache Jawad <jawad.ahmindache@gmail.com>
2024-09-16 02:33:40 +0200

    [BACK] Join & Cancel association application

--- back-core/src/association-applications/association-applications.controller.ts
+++ back-core/src/association-applications/association-applications.controller.ts
@@ -1,42 +1,35 @@
-import { Controller, Post, Body, Param, Patch, Delete, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Post } from '@nestjs/common';
 import { AssociationApplicationsService } from './association-applications.service';
 import { BearAuthToken } from '../utils/decorators/BearerAuth.decorator';
-import { CurrentUserId } from '../utils/decorators/current-user-id.decorator';
 import { JoinAssociationDto } from './dto/join-association.dto';
-import { UpdateApplicationStatusDto } from './dto/update-application-status.dto';
-import { SetApplicationQuestionDto } from './dto/set-application-question.dto';
+import { CurrentUserId } from '../utils/decorators/current-user-id.decorator';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('association-applications')
-@UseGuards(BearAuthToken)
+@BearAuthToken()
+@ApiBearerAuth()
 export class AssociationApplicationsController {
-    constructor(private readonly applicationService: AssociationApplicationsService) {}
+  constructor(
+    private readonly applicationService: AssociationApplicationsService,
+  ) {}
 
- /*   @Post('join')
-    async joinAssociation(
-        @CurrentUserId() userId: string,
-        @Body() joinAssociationDto: JoinAssociationDto
-    ) {
-        return this.applicationService.joinAssociation(userId, joinAssociationDto);
-    }
+  @Post('join')
+  async joinAssociation(
+    @CurrentUserId() userId: string,
+    @Body() joinAssociationDto: JoinAssociationDto,
+  ) {
+    return this.applicationService.joinAssociation(userId, joinAssociationDto);
+  }
 
-    @Delete('cancel')
-    async cancelApplication(@CurrentUserId() userId: string) {
-        return this.applicationService.cancelApplication(userId);
-    }
+  @Delete(':id')
+  async cancelApplication(
+    @CurrentUserId() userId: string,
+    @Param('id') id: string,
+  ) {
+    return this.applicationService.cancelApplication(userId, id);
+  }
 
-    @Patch(':id/status')
-    async updateApplicationStatus(
-        @Param('id') id: string,
-        @Body() updateStatusDto: UpdateApplicationStatusDto
-    ) {
-        return this.applicationService.updateApplicationStatus(id, updateStatusDto);
-    }
-
-    @Patch(':associationId/question')
-    async setApplicationQuestion(
-        @Param('associationId') associationId: string,
-        @Body() setQuestionDto: SetApplicationQuestionDto
-    ) {
-        return this.applicationService.setApplicationQuestion(associationId, setQuestionDto);
-    }*/
+  /**
+   * @todo Add denyApplication and acceptApplication routes
+   */
 }
--- back-core/src/association-applications/association-applications.service.ts
+++ back-core/src/association-applications/association-applications.service.ts
@@ -1,119 +1,127 @@
-import {Injectable, ConflictException, NotFoundException, Inject} from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
+import {
+  ConflictException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Repository } from 'typeorm';
-import { AssociationApplication, ApplicationStatus } from './entities/association-application.entity';
+import {
+  ApplicationStatus,
+  AssociationApplication,
+} from './entities/association-application.entity';
 import { User } from '../users/entities/user.entity';
 import { Association } from '../associations/entities/association.entity';
 import { JoinAssociationDto } from './dto/join-association.dto';
-import { UpdateApplicationStatusDto } from './dto/update-application-status.dto';
-import { SetApplicationQuestionDto } from './dto/set-application-question.dto';
 
 @Injectable()
 export class AssociationApplicationsService {
-    constructor(
-        @Inject('ASSOCIATION_APPLICATION_REPOSITORY')
-        private readonly associationApplicationRepository: Repository<AssociationApplication>,
-        @Inject('USER_REPOSITORY')
-        private readonly userRepository: Repository<User>,
-        @Inject('ASSOCIATION_REPOSITORY')
-        private readonly associationRepository: Repository<Association>,
-    ) {}
+  constructor(
+    @Inject('ASSOCIATION_APPLICATION_REPOSITORY')
+    private readonly associationApplicationRepository: Repository<AssociationApplication>,
+    @Inject('USER_REPOSITORY')
+    private readonly userRepository: Repository<User>,
+    @Inject('ASSOCIATION_REPOSITORY')
+    private readonly associationRepository: Repository<Association>,
+  ) {}
 
-    /*async joinAssociation(userId: string, joinAssociationDto: JoinAssociationDto) {
-        const { associationId, applicationAnswer } = joinAssociationDto;
-        const user = await this.userRepository.findOne({
-            where: { id: userId },
-            relations: ['association'],
-        });
-        const association = await this.associationRepository.findOne({ where: { id: associationId } });
+  async joinAssociation(
+    userId: string,
+    joinAssociationDto: JoinAssociationDto,
+  ) {
+    const { associationId, applicationAnswer } = joinAssociationDto;
+    const user = await this.userRepository.findOne({
+      where: { id: userId },
+      relations: ['associations'],
+    });
+    const association = await this.associationRepository.findOne({
+      where: { id: associationId },
+    });
 
-        if (!user || !association)
-            throw new NotFoundException('User or Association not found');
+    if (!user || !association)
+      throw new NotFoundException(
+        "L'utilisateur ou l'association n'a pas été trouvé(e)",
+      );
 
-        if (user.association)
-            throw new ConflictException({
-                message: `User is already in association ${user.association.name}`,
-                associationId: user.association.id
-            });
-
-        const existingApplication = await this.associationApplicationRepository.findOne({
-            where: {
-                user: { id: userId },
-                status: ApplicationStatus.IN_PROGRESS
-            },
-            relations: ['association'],
-        });
-
-
-        if (existingApplication)
-            throw new ConflictException({
-                message: `A request is already in progress for association ${existingApplication.association.name}`,
-                associationId: existingApplication.association.id
-            });
-
-        const application = new AssociationApplication();
-        application.user = user;
-        application.association = association;
-        application.applicationAnswer = applicationAnswer;
-        application.status = ApplicationStatus.IN_PROGRESS;
-
-        return this.associationApplicationRepository.save(application);
+    if (user.associations.some((a) => a.id === associationId)) {
+      throw new ConflictException({
+        message: `Vous êtes déjà dans cette association`,
+      });
     }
 
-    async cancelApplication(userId: string) {
-        const application = await this.associationApplicationRepository.findOne({
-            where: { user: { id: userId }, status: ApplicationStatus.IN_PROGRESS },
-        });
+    const existingApplication =
+      await this.associationApplicationRepository.findOne({
+        where: {
+          user: { id: userId },
+          association: { id: associationId },
+          status: ApplicationStatus.IN_PROGRESS,
+        },
+      });
 
-        if (!application)
-            throw new NotFoundException('No ongoing application found for this user');
+    if (existingApplication)
+      throw new ConflictException({
+        message: `Vous avez déjà demandé à joindre cette association`,
+      });
 
-        await this.associationApplicationRepository.remove(application);
-        return { message: 'Application cancelled successfully' };
-    }
+    const application = new AssociationApplication();
+    application.user = user;
+    application.association = association;
+    application.applicationQuestion = association.applicationQuestion;
+    application.applicationAnswer = applicationAnswer;
+    application.status = ApplicationStatus.IN_PROGRESS;
+    return this.associationApplicationRepository.save(application);
+  }
 
-    async updateApplicationStatus(applicationId: string, updateStatusDto: UpdateApplicationStatusDto) {
-        const { status, applicationAnswer } = updateStatusDto;
-        const application = await this.associationApplicationRepository.findOne({
-            where: { id: applicationId },
-            relations: ['user', 'association'],
-        });
+  async cancelApplication(userId: string, id: string) {
+    const application = await this.associationApplicationRepository.findOne({
+      where: {
+        id: id,
+        user: { id: userId },
+        status: ApplicationStatus.IN_PROGRESS,
+      },
+    });
 
-        if (!application)
-            throw new NotFoundException('Application not found');
+    if (!application)
+      throw new NotFoundException(
+        'La demande n’a pas été trouvée ou a déjà été traitée',
+      );
 
-        application.status = status;
-        if (applicationAnswer)
-            application.applicationAnswer = applicationAnswer;
+    await this.associationApplicationRepository.remove(application);
+    return { message: 'Application cancelled successfully' };
+  }
+  /*
+        async updateApplicationStatus(applicationId: string, updateStatusDto: UpdateApplicationStatusDto) {
+            const { status, applicationAnswer } = updateStatusDto;
+            const application = await this.associationApplicationRepository.findOne({
+                where: { id: applicationId },
+                relations: ['user', 'association'],
+            });
 
-        if (status === ApplicationStatus.APPROVED) {
-            application.user.association = application.association;
-            await this.userRepository.save(application.user);
+            if (!application)
+                throw new NotFoundException('Application not found');
+
+            application.status = status;
+            if (applicationAnswer)
+                application.applicationAnswer = applicationAnswer;
+
+            if (status === ApplicationStatus.APPROVED) {
+                application.user.association = application.association;
+                await this.userRepository.save(application.user);
+            }
+
+            return this.associationApplicationRepository.save(application);
         }
 
-        return this.associationApplicationRepository.save(application);
-    }
+        async getApplicationsByAssociation(associationId: string) {
+            return this.associationApplicationRepository.find({
+                where: { association: { id: associationId } },
+                relations: ['user'],
+            });
+        }
 
-    async setApplicationQuestion(associationId: string, setQuestionDto: SetApplicationQuestionDto) {
-        const association = await this.associationRepository.findOne({ where: { id: associationId } });
-        if (!association)
-            throw new NotFoundException('Association not found');
-
-        association.applicationQuestion = setQuestionDto.question;
-        return this.associationRepository.save(association);
-    }
-
-    async getApplicationsByAssociation(associationId: string) {
-        return this.associationApplicationRepository.find({
-            where: { association: { id: associationId } },
-            relations: ['user'],
-        });
-    }
-
-    async getApplicationsByUser(userId: string) {
-        return this.associationApplicationRepository.find({
-            where: { user: { id: userId } },
-            relations: ['association'],
-        });
-    }*/
+        async getApplicationsByUser(userId: string) {
+            return this.associationApplicationRepository.find({
+                where: { user: { id: userId } },
+                relations: ['association'],
+            });
+        }*/
 }
